0cc4399dabb9678f2f15b3c1d6883f07ab6be106
= <=>
2023-07-30 12:22:10 +0100

    Link

--- src/App.js
+++ src/App.js
@@ -26,10 +26,10 @@ export default function App() {
       <Appbar/>
       <Container>
         <Routes>
-            <Route path='/data' exact index element={<Home/>} />
+            <Route path='/' exact index element={<Home/>} />
             <Route path='/*' exact index element={<Home/>} />
-            <Route path='/data/Courses' element={<Courses/>} />
-            <Route path='/data/About' element={<About/>} />
+            <Route path='/Courses' element={<Courses/>} />
+            <Route path='/About' element={<About/>} />
             <Route path='/data/Our-Services' element={<Services/>} />
             <Route path='/Data-Analysis-Services' element={<DataServices/>} />
             <Route path='/Software-Development-Services' element={<WebDev/>} />
