329bb182a47149b8ec084e35921b98d046db7e62
= <=>
2023-07-30 12:59:12 +0100

    Link

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'slick-carousel/slick/slick-theme.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename='/data'>
       <App/>
     </HashRouter>
   </React.StrictMode>
