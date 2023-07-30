f7f2d574449ed33d3518910b48fa28ac411a7bc9
= <=>
2023-07-30 08:48:21 +0100

    Route Commit

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default function App() {
     <>
       <GlobalStyle/>
       <Appbar/>
+      <Home/>
       <Container>
         <Routes>
             <Route path='/' exact index element={<Home/>} />
