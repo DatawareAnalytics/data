1a2b37fe6455f138a43bf5c727ee49171e55e36e
= <=>
2023-07-30 10:16:57 +0100

    Routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
        <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
