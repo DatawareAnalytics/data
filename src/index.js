0611fde99f37ebd51cd0cd3689533271963cd1f1
= <=>
2023-07-30 12:45:04 +0100

    Hash

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter,RouterProvider, createHashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-const router = createHashRouter([
-  {
-    path:'/*',
-    element:<App/>
-  }
-])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App/>
+    </HashRouter>
   </React.StrictMode>
 );
