6d2e8be415e60cf548cd9efc93ff50f6fcba8db1
= <=>
2023-07-30 11:03:26 +0100

    Hash

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ import 'slick-carousel/slick/slick-theme.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/data'>
-       <App />
+    <BrowserRouter>
+      <HashRouter basename='/'>
+        <App />
+      </HashRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
