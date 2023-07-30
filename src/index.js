25f0609b8bdbc427f0271e176f6169de044329db
= <=>
2023-07-30 14:54:02 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
@@ -9,8 +9,8 @@ import 'slick-carousel/slick/slick-theme.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/data' >
       <App/>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
