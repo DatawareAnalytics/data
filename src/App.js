d67fc4c453ca6fded63ce73b95e53e5212dd8c20
= <=>
2023-07-30 19:03:40 +0100

    SSS

--- src/App.js
+++ src/App.js
@@ -27,9 +27,9 @@ export default function App() {
       <Container>
         <Routes>
             <Route path='/' exact index element={<Home/>} />
-            <Route path='/data/Courses' element={<Courses/>} />
-            <Route path='/data/About' element={<About/>} />
-            <Route path='/data/Our-Services' element={<Services/>} />
+            <Route path='/Courses' element={<Courses/>} />
+            <Route path='/About' element={<About/>} />
+            <Route path='/Our-Services' element={<Services/>} />
             <Route path='/Data-Analysis-Services' element={<DataServices/>} />
             <Route path='/Software-Development-Services' element={<WebDev/>} />
             <Route path='/AI-Services' element={<AI/>} />
